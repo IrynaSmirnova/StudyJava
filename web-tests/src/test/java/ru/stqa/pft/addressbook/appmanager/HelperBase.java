d5bad81a2223c269bf200712a6722cc68ba0e574
Iryna Smirnova <irena.smirnowa@gmail.com>
2019-10-11 12:39:12 -0400

    Update HelperBase.java

--- web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -1,13 +1,32 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelperBase {
     protected WebDriver driver;
 
     public HelperBase(WebDriver driver) {
         this.driver = driver;
+    }
+
+    protected WebElement waitElement(By locator, long timeOutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutSeconds);
+        wait.withMessage("Element didn't appear after timeout" + timeOutSeconds);
+        return wait.until(
+                ExpectedConditions.elementToBeClickable(locator)
+        );
+
+    }
+
+    protected void waitElementAndClick(By locator, long timeOutSeconds) {
+        WebElement element = waitElement(locator, timeOutSeconds);
+        element.click();
+
     }
 
     protected void click(By locator) {
@@ -25,5 +44,14 @@ public class HelperBase {
 
         }
 
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
     }
 }
