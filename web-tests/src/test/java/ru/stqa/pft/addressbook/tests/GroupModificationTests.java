b070daad99943c09410ba0ee346ff5dfee4adc0e
Iryna Smirnova <irena.smirnowa@gmail.com>
2019-10-11 12:38:54 -0400

    Update GroupModificationTests.java

--- web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,18 +1,28 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification () {
-        app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().selectGroup();
-        app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test", "test1", "test2"));
-        app.getGroupHelper().submitGroupModification();
-        app.getGroupHelper().returnToGroupPage();
+        app.navigationBar().clickOnGroups();
+
+        if (! app.groupPage().isThereAGroup()); {
+            app.groupPage().createGroup(new GroupData("test", null, null));
+        }
+        List<GroupData> before = app.groupPage().groupList();
+        app.groupPage().selectGroup(before.size()- 1);
+        app.groupPage().initGroupModification();
+        app.groupPage().fillGroupForm(new GroupData("test", "test1", "test2"));
+        app.groupPage().submitGroupModification();
+        app.groupPage().returnToGroupPage();
+        List<GroupData> after = app.groupPage().groupList();
+        Assert.assertEquals(after.size(), before.size());
 
     }
 
